4d0cd82897411cc657eea341fb7adfa6c126f04d
ryo <fry3292@gmail.com>
2023-02-22 00:06:46 +0900

    init

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// socket
+io.on('connection', function (socket) {
+
+  // 部屋に対しテキストを送る
+  socket.on('sendMessage', function (param) {
+    const {text} = param;
+
+    //　送信する。 socket.on('getMessage')で取得できる
+    io.emit('getMessage', text);
+  });
+
+  /**
+   * 
+   * 部屋の参加と、部屋限定の送信
+   * 
+   */
+  // 部屋に参加する
+  socket.on('joinRoom', function (param) {
+    // 配列可能
+    const room_keys = param;
+    socket.join(room_keys);
+  });
+  // 部屋を抜ける
+  socket.on('leaveRoom', function (param) {
+    // // 配列可能
+    // const room_keys = param;
+    // socket.leave(room_keys)
+
+    socket.leaveAll()
+  });
+
+  // 部屋に対しテキストを送る
+  socket.on('sendMessageRoom', function (param) {
+    const {room_key, text} = param;
+
+    // 上でjoinした人だけが、 socket.on('getMessageRoom')で取得できる
+    io.to(room_key).emit('getMessageRoom', text);
+  });
+
+});
+
+
+
+// localhostを起動する
+http.listen(3000, function () {
+  console.log('server start @3000 port');
+});
+
+
+// http://localhost:3000/ で　index.htmlを表示する
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/main.html', function (req, res) {
+  res.sendFile(__dirname + '/main.html');
+});
---
+++ readme.md
@@ -0,0 +1,25 @@
+# Install
+
+**install node.js**
+
+``` bash
+cd /〇〇/nodejs_socketio
+npm install
+```
+
+**install SocketIO**
+
+``` bash
+npm install --save express socket.io
+```
+
+**Run**
+
+``` bash
+node index.js
+```
+
+**Confirm**
+
+http://localhost:3000/
+
